3c3609a72f29b6097992beeafb4c2db24abc295c
hrshdtt <47847285+hrshdtt@users.noreply.github.com>
2021-02-21 16:47:54 +0530

    Update solver2.m

--- solver2.m
+++ solver2.m
@@ -1,5 +1,8 @@
 % runge-kutta method for ode
+h = hj 
 f2a = 0
 f2b = 0
+%begin loop
 if (R + h)*sin(theta) >= R
-  f2a = 
+  f2a = 2*pi - 8*alpha*(sin(theta)^2)*(R + h)*sin(theta)
+  f2b = 4*R*cos(theta)*sqrt(((R + h)*sin(theta))^2 - R^2) - 12*alpha*(((R + h)*sin(theta))^2)*cos(theta) + pi*(2*R*h + h*h)*cos(theta)
